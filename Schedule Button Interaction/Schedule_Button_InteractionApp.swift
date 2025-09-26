69e84238a8fa7a64baaa39fdd48efcf83e7e826a
ledoux25 <sanguojoseph17@gmail.com>
2025-09-26 15:40:27 -0400

    Initial Commit

---
+++ Schedule Button Interaction/Schedule_Button_InteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schedule_Button_InteractionApp.swift
+//  Schedule Button Interaction
+//
+//  Created by Sanguo Joseph Ledoux on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Schedule_Button_InteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Schedule Button InteractionTests/Schedule_Button_InteractionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Schedule_Button_InteractionTests.swift
+//  Schedule Button InteractionTests
+//
+//  Created by Sanguo Joseph Ledoux on 9/26/25.
+//
+
+import Testing
+@testable import Schedule_Button_Interaction
+
+struct Schedule_Button_InteractionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
